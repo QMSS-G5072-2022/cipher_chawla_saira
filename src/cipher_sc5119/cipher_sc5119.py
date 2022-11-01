6fdcfb54e64381bfe08e99860c4bb06352839c96
sairachawla <sairachawla@berkeley.edu>
2022-11-01 18:23:48 -0400

    add function to src + docstring

---
+++ src/cipher_sc5119/cipher_sc5119.py
@@ -0,0 +1,28 @@
+## importing necessary libraries 
+
+def cipher(text, shift, encrypt=True):
+    '''
+    function encrypts or decrypts a string based in the shift valye
+    takes in 3 parameters: a string, an integer to shift the string by, and a boolean that stands for encrypt or decrypt
+    example 1:
+        text = dog
+        shift = 2
+        encrypt = True 
+            returns -> fqi
+    example 2:
+        text = fqi
+        shift = 2
+        encrypt = False
+            returns -> dog
+    '''
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text

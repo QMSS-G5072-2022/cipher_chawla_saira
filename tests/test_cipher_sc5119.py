8fb161585ec77076ece565a715dff23bce5d04f8
sairachawla <sairachawla@berkeley.edu>
2022-11-01 18:31:26 -0400

    add tests

--- tests/test_cipher_sc5119.py
+++ tests/test_cipher_sc5119.py
@@ -1,1 +1,8 @@
 from cipher_sc5119 import cipher_sc5119
+
+def test_cipher():
+	example = "quantitative"
+	shift = 1
+	expected = "rvboujubujwf"
+	actual = cipher_sc5119.cipher(example, shift)
+	assert actual == expected, "should be rvboujubujwf"
